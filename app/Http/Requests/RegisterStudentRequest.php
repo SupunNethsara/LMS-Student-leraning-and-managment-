16a1e00eae42db88f8c0c0f61b8424e751499135
Supun Nethsara <supunmax663@gmail.com>
2025-04-06 22:56:59 -0700

    Moved changes from main to changes branch

--- app/Http/Requests/RegisterStudentRequest.php
+++ app/Http/Requests/RegisterStudentRequest.php
@@ -6,23 +6,28 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterStudentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+   
     public function authorize(): bool
     {
         return false;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+  
     public function rules(): array
     {
         return [
-            //
+            'fname' => 'required|',
+            'mname' => 'nullable',
+            'lname' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|',
+            'corce' => 'nullable|',
+            'gender' => 'required|',
+            'contact' => 'nullable|',
+            'qulification' => 'nullable|',
+            'role' => 'nullable|',
+            'adress' => 'nullable|',
+            'profile' => 'nullable|url' 
         ];
     }
 }
